93fd5736cefd49fae42b9e23ff2d35d4519f8777
Ryula <hobby519@naver.com>
2023-12-29 02:03:52 +0900

    [feat] init

---
+++ src/main/java/com/gradu/infou/InfouApplication.java
@@ -0,0 +1,13 @@
+package com.gradu.infou;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InfouApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InfouApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/gradu/infou/InfouApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gradu.infou;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InfouApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
