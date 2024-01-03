74193f0b1cd6b4f64dc9ba7fd64a1c391e4294d9
Ryula <hobby519@naver.com>
2024-01-03 23:45:07 +0900

    [test] git conflict test

--- src/main/java/com/gradu/infou/Controller/PortalController.java
+++ src/main/java/com/gradu/infou/Controller/PortalController.java
@@ -1,4 +1,12 @@
 package com.gradu.infou.Controller;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
 public class PortalController {
+    @GetMapping("/search")
+    public String search(){
+        return "search";
+    }
 }
