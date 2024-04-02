86f6dc85e8b789b49dfb2050a6c390042f33a04b
Ryula <hobby519@naver.com>
2024-04-02 19:31:30 +0900

    feat: 비슷한 사용자의 infou 검색 추가

--- src/main/java/com/gradu/infou/Service/ElasticQueryService.java
+++ src/main/java/com/gradu/infou/Service/ElasticQueryService.java
@@ -1,9 +1,12 @@
 package com.gradu.infou.Service;
 
+import com.gradu.infou.Config.exception.BaseException;
 import com.gradu.infou.Domain.Dto.Controller.Condition;
 import com.gradu.infou.Domain.Dto.Controller.Kind;
 import com.gradu.infou.Domain.Dto.Controller.PortalSearchAggregationResult;
 import com.gradu.infou.Domain.Dto.Service.SearchLectureResDto;
+import com.gradu.infou.Domain.Entity.InfouProcessDocument;
+import com.gradu.infou.Repository.InfouProcessRepository;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.elasticsearch.action.search.SearchRequest;
@@ -18,6 +21,7 @@ import org.elasticsearch.search.aggregations.BucketOrder;
 import org.elasticsearch.search.aggregations.bucket.terms.Terms;
 import org.elasticsearch.search.aggregations.bucket.terms.TermsAggregationBuilder;
 import org.elasticsearch.search.aggregations.metrics.Avg;
+import org.elasticsearch.search.aggregations.metrics.ValueCount;
 import org.elasticsearch.search.builder.SearchSourceBuilder;
 import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
@@ -26,6 +30,9 @@ import java.io.IOException;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
+import java.util.Optional;
+
+import static com.gradu.infou.Config.BaseResponseStatus.NOT_FOUND_INFOU;
 
 @Slf4j
 @Service
@@ -33,6 +40,7 @@ import java.util.List;
 public class ElasticQueryService {
 
     private final RestHighLevelClient elasticsearchClient;
+    private final InfouProcessRepository infouProcessRepository;
 
 
     public SearchResponse searchWithAggregations(String keyword, Kind condition, String[] sort, Pageable pageable, String index) throws IOException {
@@ -89,6 +97,69 @@ public class ElasticQueryService {
             SearchLectureResDto result = new SearchLectureResDto(s[0],s[1],s[2],s[3], avgValue);
 
             results.add(result);
+        }
+        return results;
+    }
+
+    public SearchResponse searchRecommendWithAggregations(String grade, String department, String keyword, String[] sort, Pageable pageable) throws IOException {
+
+        int totalSize = pageable.getPageSize() * (pageable.getPageNumber() + 1);
+        boolean asc = true;
+
+        if(sort[1].equals("desc")||sort[1].equals("DESC")) asc = false;
+
+        TermsAggregationBuilder aggregation = AggregationBuilders.terms("group_by_fields")
+                .script(new Script(ScriptType.INLINE, "painless", "doc['id.keyword'].value", new HashMap<>()))
+                .size(totalSize)
+                .order(BucketOrder.aggregation(sort[0], asc))
+                .subAggregation(AggregationBuilders.count(sort[0]).field("message"));
+
+        SearchSourceBuilder searchSourceBuilder = new SearchSourceBuilder();
+
+        // 검색 쿼리 설정
+        searchSourceBuilder.query(QueryBuilders.boolQuery()
+                .filter(QueryBuilders.existsQuery("grade"))
+                .filter(QueryBuilders.existsQuery("department"))
+                .must(QueryBuilders.termQuery("message", keyword))
+                .must(QueryBuilders.termQuery("grade",grade))
+                .must(QueryBuilders.termQuery("department",department))
+        );
+
+        // 집계 설정
+        searchSourceBuilder.aggregation(aggregation);
+
+        SearchRequest searchRequest = new SearchRequest("log"); // index name
+        searchRequest.source(searchSourceBuilder);
+
+        SearchResponse search = elasticsearchClient.search(searchRequest, RequestOptions.DEFAULT);
+
+        return search;
+    }
+
+    public List<InfouProcessDocument> searchRecommend(String grade, String department, String keyword, Pageable pageable) throws IOException {
+        String[] sort = pageable.getSort().toString().split(": ");
+        log.info("1: "+sort[0]);
+        log.info("2: "+sort[1]);
+        SearchResponse searchResponse = searchRecommendWithAggregations(grade, department, keyword, sort, pageable);
+
+        Terms termsAggregation = searchResponse.getAggregations().get("group_by_fields");
+
+        List<InfouProcessDocument> results = new ArrayList<>();
+
+        int i=0;
+        int size = pageable.getPageSize();
+        int page = pageable.getPageNumber();
+
+        for (Terms.Bucket bucket : termsAggregation.getBuckets()) {
+            if(i++<size*page) continue;
+            String key = bucket.getKeyAsString(); // 버킷의 키
+
+            InfouProcessDocument processDocument = infouProcessRepository.findById(key).orElseThrow(()->new BaseException(NOT_FOUND_INFOU));
+
+            ValueCount count = bucket.getAggregations().get(sort[0]);
+//            Long countValue = count.getValue(); // 평균 값
+
+            results.add(processDocument);
         }
         return results;
     }
